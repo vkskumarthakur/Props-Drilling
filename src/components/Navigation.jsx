21c84f19613963f4767121f08943ec2dfde9dd7a
vikas <vkskumarthakur@gmail.com>
2024-07-02 01:01:31 +0530

    commet added

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,6 +4,7 @@ const Navigation = (props) => {
     console.log(props);
   return (
     <div>
+      {/* here we are using the props data from the app.jsx */}
         This is {props.userDetails.name} <br/>
         This is {props.userDetails.address}
     </div>
